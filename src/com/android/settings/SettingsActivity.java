omnirom/android_packages_apps_Settings

Settings: Omnigears initial checkin
Change-Id: Iab7c11d3bba14bd7c9a6b354af6fda63f7228ce0

src/com/android/settings/SettingsActivity.java
@@ -132,6 +132,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.omnirom.omnigears.OmniGearsSettings;
+
 public class SettingsActivity extends SettingsDrawerActivity
         implements PreferenceManager.OnPreferenceTreeClickListener,
         PreferenceFragment.OnPreferenceStartFragmentCallback,
@@ -345,6 +347,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             TestingSettings.class.getName(),
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
+            OmniGearsSettings.class.getName(),
     };
 
 
@@ -542,6 +545,7 @@ public class SettingsActivity extends SettingsDrawerActivity
 
         mIsShowingDashboard = className.equals(Settings.class.getName())
                 || className.equals(Settings.WirelessSettings.class.getName())
+                || className.equals(Settings.OmniGearsSettings.class.getName())
                 || className.equals(Settings.DeviceSettings.class.getName())
                 || className.equals(Settings.PersonalSettings.class.getName())
                 || className.equals(Settings.WirelessSettings.class.getName());

LuanNg/packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -139,6 +139,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.omnirom.omnigears.OmniGearsSettings;
+
 public class SettingsActivity extends SettingsDrawerActivity
         implements PreferenceManager.OnPreferenceTreeClickListener,
         PreferenceFragment.OnPreferenceStartFragmentCallback,
@@ -359,7 +361,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
-            AutomaticStorageManagerSettings.class.getName()
+            AutomaticStorageManagerSettings.class.getName(),
+            OmniGearsSettings.class.getName()
     };
 
 
@@ -551,6 +554,7 @@ public class SettingsActivity extends SettingsDrawerActivity
 
         mIsShowingDashboard = className.equals(Settings.class.getName())
                 || className.equals(Settings.WirelessSettings.class.getName())
+                || className.equals(Settings.OmniGearsSettings.class.getName())
                 || className.equals(Settings.DeviceSettings.class.getName())
                 || className.equals(Settings.PersonalSettings.class.getName())
                 || className.equals(Settings.WirelessSettings.class.getName());